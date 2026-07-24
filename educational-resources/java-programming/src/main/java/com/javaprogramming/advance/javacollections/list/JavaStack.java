3005648e5755584277a401f408c5562363f85bd2
Avinash Nishad <js.avinash.nishad@gmail.com>
2026-07-24 16:13:58 +0530

    feat(java) collection examples

--- educational-resources/java-programming/src/main/java/com/javaprogramming/advance/javacollections/list/JavaStack.java
+++ educational-resources/java-programming/src/main/java/com/javaprogramming/advance/javacollections/list/JavaStack.java
@@ -1,3 +1,16 @@
 package com.javaprogramming.advance.javacollections.list;
 
-public class JavaStack {}
+import java.util.Stack;
+
+public class JavaStack {
+  public static void main(String[] args) {
+    Stack<String> s = new Stack<>();
+    s.add("apple");
+    s.add("mango");
+    System.out.println(s);
+    s.pop();
+    System.out.println(s);
+    s.push("dates");
+    System.out.println(s);
+  }
+}
--- educational-resources/java-programming/src/main/java/com/javaprogramming/advance/javacollections/list/JavaVector.java
+++ educational-resources/java-programming/src/main/java/com/javaprogramming/advance/javacollections/list/JavaVector.java
@@ -1,3 +1,14 @@
 package com.javaprogramming.advance.javacollections.list;
 
-public class JavaVector {}
+import java.util.Vector;
+
+public class JavaVector {
+  public static void main(String[] args) {
+    Vector<String> v = new Vector<>();
+    v.add("apple");
+    v.add("mango");
+    for (String item : v) {
+      System.out.println(item);
+    }
+  }
+}
